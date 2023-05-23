b329942533d5dfe67a04f16d0c7833bc4cd3fb33
Howbe <109429949+HowbeTD@users.noreply.github.com>
2023-05-23 14:00:44 -0400

    1.7
    Testing git pull to hosting

--- src/commands/tools/ping.js
+++ src/commands/tools/ping.js
@@ -9,7 +9,7 @@ module.exports = {
             fetchReply: true
         });
 
-        const newMessage = `API Latency: ${client.ws.ping}\nClient Ping: ${message.createdTimestamp - interaction.createdTimestamp}`
+        const newMessage = `Hi! \nAPI Latency: ${client.ws.ping}\nClient Ping: ${message.createdTimestamp - interaction.createdTimestamp}`
         await interaction.editReply({
             content: newMessage
         });
