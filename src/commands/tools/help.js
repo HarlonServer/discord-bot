ac441ec6d0ccc07d01698f00cd48f1f4aaff6f50
Howbe <109429949+HowbeTD@users.noreply.github.com>
2023-06-24 16:52:37 -0400

    1.9.7

--- src/commands/tools/help.js
+++ src/commands/tools/help.js
@@ -20,9 +20,33 @@ module.exports = {
   async execute(interaction, client) {
     const topic = interaction.options.getString('topic');
     const housingEmbed = new EmbedBuilder()
-        .setTitle(`Helper`)
+        .setTitle(`**Q:** How can I get a house/apartment?`)
         .setDescription(
-          "**Q:** How can I get a house/apartment?\n\n**A:**\n\n🏘️ **Free Public Housing** can be found in Shoretrench. It can be claimed by clicking on a \"For Sale\" sign on the outside of a unit.\n\nWhen arriving by train, follow the markers in the ground, which will lead you to available units.\n\n🚇 [**Trip Planner** to Shoretrench](https://harlontripplanner.muffinbardeyt.repl.co/d/publichousing)\n**🧭 GPS** to Shoretrench: ``/gps start hl-shoretrench``\n\n🏘️ If you wish to buy an **apartment** or **house** elsewhere, you can do so anywhere across Harlon by simply clicking on a \"For Sale\" sign if one is available. If one is not available at the property you are interested in, please ask Staff either in-game or open a ticket in <#721631119421734982>."
+          "🏘️ **Free Public Housing** can be found in Shoretrench. It can be claimed by clicking on a \"For Sale\" sign on the outside of a unit.\n\nWhen arriving by train, follow the markers in the ground, which will lead you to available units.\n\n🚇 [**Trip Planner** to Shoretrench](https://harlontripplanner.muffinbardeyt.repl.co/d/publichousing)\n**🧭 GPS** to Shoretrench: ``/gps start hl-shoretrench``\n\n🏘️ If you wish to buy an **apartment** or **house** elsewhere, you can do so anywhere across Harlon by simply clicking on a \"For Sale\" sign if one is available. If one is not available at the property you are interested in, please ask Staff either in-game or open a ticket in <#721631119421734982>."
+        )
+        .setColor(0x00f51d)
+        .setThumbnail(client.user.displayAvatarURL())
+        .setTimestamp(Date.now())
+    const factoryEmbed = new EmbedBuilder()
+        .setTitle(`**Q:** How can I buy a factory?`)
+        .setDescription(
+          "🏭 **Factories** can be bought in the Portsfield industrial area for £1000HLP. You can purchase a factory by clicking on a \"For Sale\" sign at the entrance to one. \n\n**🚇 Trip Planner** to Portsfield: <https://harlontripplanner.muffinbardeyt.repl.co/t/?c=wKt>\n**🧭 GPS** to Portsfield: ``/gps start hl-portsfield``\n\n📈 You can upgrade your factory to a **Level 2** factory by asking Staff in-game or opening a ticket in <#721631119421734982>."
+        )
+        .setColor(0x00f51d)
+        .setThumbnail(client.user.displayAvatarURL())
+        .setTimestamp(Date.now())
+    const materialsEmbed = new EmbedBuilder()
+        .setTitle(`**Q:** How do I gather materials?`)
+        .setDescription(
+          "⛏️ You can go mining at the **Hibbing Ore Mine** to gather resources. \n\n🚇 [**Trip Planner** to Hibbing Ore Mine](https://harlontripplanner.muffinbardeyt.repl.co/d/oremines)\n\n🌲 You can cut trees at the **Roslagen Tree Plantation**.\n\n🚇 [**Trip Planner** to Roslagen Tree Plantation](https://harlontripplanner.muffinbardeyt.repl.co/d/treeplantation)\n🧭 **GPS** to Roslagen Tree Plantation: ``/gps start hl-treeplantation``"
+        )
+        .setColor(0x00f51d)
+        .setThumbnail(client.user.displayAvatarURL())
+        .setTimestamp(Date.now())
+    const shopsEmbed = new EmbedBuilder()
+        .setTitle(`**Q:** How do I start a shop?`)
+        .setDescription(
+          "🛍️ You can **buy/rent shop spaces** across the city by clicking on a \"For Sale/Rent\" sign at the property. You can request Staff to add exterior branding and add your shop to the Dynmap for greater visibility. \n\n💰 Set up the selling chests: **1:** Left-click a chest with the item you wish to sell. **2:** Write in the cost per item. **3:** Add items to the chest and you are good to go! **4:** If you wish to edit your chest (price etc.), simply right-click the sign."
         )
         .setColor(0x00f51d)
         .setThumbnail(client.user.displayAvatarURL())
@@ -30,20 +54,11 @@ module.exports = {
     if (topic === 'housing') {
         await interaction.reply({embeds: [housingEmbed]});
     } else if (topic === 'factory') {
-        await interaction.reply({
-            content: "## **Q:** How can I buy a factory?\n\n🏭 **Factories** can be bought in the Portsfield industrial area for £1000HLP. You can purchase a factory by clicking on a \"For Sale\" sign at the entrance to one. \n\n**🚇 Trip Planner** to Portsfield: <https://harlontripplanner.muffinbardeyt.repl.co/t/?c=wKt>\n**🧭 GPS** to Portsfield: ``/gps start hl-portsfield``\n\n📈 You can upgrade your factory to a **Level 2** factory by asking Staff in-game or opening a ticket in <#721631119421734982>.",
-            ephemeral: false,
-        });
+        await interaction.reply({embeds: [factoryEmbed]});
     } else if (topic === 'materials') {
-        await interaction.reply({
-            content: "**Q:** How do I gather materials?\n\n**A:**\n\n⛏️ You can go mining at the **Hibbing Ore Mine** to gather resources. \n\n🚇 [**Trip Planner** to Hibbing Ore Mine](https://harlontripplanner.muffinbardeyt.repl.co/d/oremines)\n\n🌲 You can cut trees at the **Roslagen Tree Plantation**.\n\n🚇 [**Trip Planner** to Roslagen Tree Plantation](https://harlontripplanner.muffinbardeyt.repl.co/d/treeplantation)\n🧭 **GPS** to Roslagen Tree Plantation: ``/gps start hl-treeplantation``",
-            ephemeral: false,
-        });
+        await interaction.reply({embeds: [materialsEmbed]});
     } else if (topic === 'shops') {
-        await interaction.reply({
-            content: "**Q:** How do I start a shop?\n\n**A:**\n\n🛍️ You can **buy/rent shop spaces** across the city by clicking on a \"For Sale/Rent\" sign at the property. You can request Staff to add exterior branding and add your shop to the Dynmap for greater visibility. \n\n💰 Set up the selling chests: **1:** Left-click a chest with the item you wish to sell. **2:** Write in the cost per item. **3:** Add items to the chest and you are good to go! **4:** If you wish to edit your chest (price etc.), simply right-click the sign.",
-            ephemeral: false,
-        });
+        await interaction.reply({embeds: [shopsEmbed]});
     };
   },
 };
